66ca10d43dde163446fd1399fc12202d614594d9
msellamiTN <mokhtar.sellami@gmail.com>
2022-10-04 12:36:02 +0100

    Update models.py

--- src/models.py
+++ src/models.py
@@ -30,59 +30,60 @@ class TFParts(object):
         self._prior_psl = 0
 
     def build_basics(self):
-        tf.reset_default_graph()
-        with tf.variable_scope("graph", initializer=tf.truncated_normal_initializer(0, 0.3)):
+        tf.compat.v1.reset_default_graph()
+        tf.compat.v1.disable_eager_execution()
+        with tf.compat.v1.variable_scope("graph", initializer=tf.compat.v1.truncated_normal_initializer(0, 0.3)):
             # Variables (matrix of embeddings/transformations)
-            self._ht = ht = tf.get_variable(
+            self._ht = ht = tf.compat.v1.get_variable(
                 name='ht',  # for t AND h
                 shape=[self.num_cons, self.dim],
                 dtype=tf.float32)
 
-            self._r = r = tf.get_variable(
+            self._r = r = tf.compat.v1.get_variable(
                 name='r',
                 shape=[self.num_rels, self.dim],
                 dtype=tf.float32)
 
-            self._A_h_index = A_h_index = tf.placeholder(
+            self._A_h_index = A_h_index = tf.compat.v1.placeholder(
                 dtype=tf.int64,
                 shape=[self.batch_size],
                 name='A_h_index')
-            self._A_r_index = A_r_index = tf.placeholder(
+            self._A_r_index = A_r_index = tf.compat.v1.placeholder(
                 dtype=tf.int64,
                 shape=[self.batch_size],
                 name='A_r_index')
-            self._A_t_index = A_t_index = tf.placeholder(
+            self._A_t_index = A_t_index = tf.compat.v1.placeholder(
                 dtype=tf.int64,
                 shape=[self.batch_size],
                 name='A_t_index')
 
             # for uncertain graph
-            self._A_w = tf.placeholder(
+            self._A_w = tf.compat.v1.placeholder(
                 dtype=tf.float32,
                 shape=[self.batch_size],
                 name='_A_w')
 
-            self._A_neg_hn_index = A_neg_hn_index = tf.placeholder(
+            self._A_neg_hn_index = A_neg_hn_index = tf.compat.v1.placeholder(
                 dtype=tf.int64,
                 shape=(self.batch_size, self._neg_per_positive),
                 name='A_neg_hn_index')
-            self._A_neg_rel_hn_index = A_neg_rel_hn_index = tf.placeholder(
+            self._A_neg_rel_hn_index = A_neg_rel_hn_index = tf.compat.v1.placeholder(
                 dtype=tf.int64,
                 shape=(self.batch_size, self._neg_per_positive),
                 name='A_neg_rel_hn_index')
-            self._A_neg_t_index = A_neg_t_index = tf.placeholder(
+            self._A_neg_t_index = A_neg_t_index = tf.compat.v1.placeholder(
                 dtype=tf.int64,
                 shape=(self.batch_size, self._neg_per_positive),
                 name='A_neg_t_index')
-            self._A_neg_h_index = A_neg_h_index = tf.placeholder(
+            self._A_neg_h_index = A_neg_h_index = tf.compat.v1.placeholder(
                 dtype=tf.int64,
                 shape=(self.batch_size, self._neg_per_positive),
                 name='A_neg_h_index')
-            self._A_neg_rel_tn_index = A_neg_rel_tn_index = tf.placeholder(
+            self._A_neg_rel_tn_index = A_neg_rel_tn_index = tf.compat.v1.placeholder(
                 dtype=tf.int64,
                 shape=(self.batch_size, self._neg_per_positive),
                 name='A_neg_rel_tn_index')
-            self._A_neg_tn_index = A_neg_tn_index = tf.placeholder(
+            self._A_neg_tn_index = A_neg_tn_index = tf.compat.v1.placeholder(
                 dtype=tf.int64,
                 shape=(self.batch_size, self._neg_per_positive),
                 name='A_neg_tn_index')
@@ -100,21 +101,21 @@ class TFParts(object):
             self._neg_tn_con_batch = tf.nn.embedding_lookup(ht, A_neg_tn_index)
 
             # psl batches
-            self._soft_h_index = tf.placeholder(
+            self._soft_h_index = tf.compat.v1.placeholder(
                 dtype=tf.int64,
                 shape=[self._soft_size],
                 name='soft_h_index')
-            self._soft_r_index = tf.placeholder(
+            self._soft_r_index = tf.compat.v1.placeholder(
                 dtype=tf.int64,
                 shape=[self._soft_size],
                 name='soft_r_index')
-            self._soft_t_index = tf.placeholder(
+            self._soft_t_index = tf.compat.v1.placeholder(
                 dtype=tf.int64,
                 shape=[self._soft_size],
                 name='soft_t_index')
 
             # for uncertain graph and psl
-            self._soft_w = tf.placeholder(
+            self._soft_w =  tf.compat.v1.placeholder(
                 dtype=tf.float32,
                 shape=[self._soft_size],
                 name='soft_w_lower_bound')
@@ -128,8 +129,8 @@ class TFParts(object):
         self._A_loss = tf.add(self.main_loss, self.psl_loss)
 
         # Optimizer
-        self._lr = lr = tf.placeholder(tf.float32)
-        self._opt = opt = tf.train.AdamOptimizer(lr)
+        self._lr = lr = tf.compat.v1.placeholder(tf.float32)
+        self._opt = opt = tf.compat.v1.train.AdamOptimizer(lr)
 
         # This can be replaced by
         # self._train_op_A = train_op_A = opt.minimize(A_loss)
@@ -138,7 +139,7 @@ class TFParts(object):
         self._train_op = opt.apply_gradients(gradient)
 
         # Saver
-        self._saver = tf.train.Saver(max_to_keep=2)
+        self._saver = tf.compat.v1.train.Saver(max_to_keep=2)
 
     def build(self):
         self.build_basics()
